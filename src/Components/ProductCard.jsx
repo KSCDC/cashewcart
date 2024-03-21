7bda7b65c17685726fed1b8b12d570c10cc6e8ca
adarshswaminath <adarshswaminath7@gmail.com>
2024-03-21 18:49:27 +0530

    feat: added rating in product cards

--- src/Components/ProductCard.jsx
+++ src/Components/ProductCard.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from 'react';
 import { BACKEND_URL } from '../constants';
 import { Link } from 'react-router-dom';
 import Loading from './Loading';
+import { IoStarSharp } from "react-icons/io5";
 
 function ProductCard({ product, selling_price,id }) {
   return (
@@ -16,16 +17,29 @@ function ProductCard({ product, selling_price,id }) {
         <h3 className="text-gray-900 font-semibold text-xl mb-2">{product.name}</h3>
         <p className="text-gray-700 text-base mb-4">{product.description.slice(0, 72)}...</p>
         <p className="text-red-500 text-xl font-semibold">₹{selling_price}</p>
-        <Link to="/purchase" state={{id:product.product_id}}>
+     
+       <div className="flex items-center justify-between">
+        {/* product rating */}
+       <div className="flex items-center text-yellow-400">
+       {Array.from({length:product.average_rating}).map((_index) => (
+          <IoStarSharp className='text-lg'/>
+        ))}
+        <span className='text-black'>({product.average_rating})</span>
+       </div>
+        {/* open product in purchase page */}
+       <Link to="/purchase" state={{id:product.product_id}}>
         <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">
           Show More 
         </button>
         
         </Link>
+       </div>
       </div>
     </div>
     </Suspense>
   );
 }
+
+// {product.average_rating}
 
 export default ProductCard;
--- src/Pages/Home/TrendingProducts.jsx
+++ src/Pages/Home/TrendingProducts.jsx
@@ -3,6 +3,7 @@ import { BACKEND_URL } from '../../constants';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import Loading from '../../Components/Loading';
 import { Link } from 'react-router-dom';
+import { IoStarSharp } from "react-icons/io5";
 
 
 function TrendingProducts() {
@@ -10,8 +11,8 @@ function TrendingProducts() {
     const [page, setPage] = useState(1);
     const [hasPrevious, setHasPrevious] = useState(false);
     const [hasNext, setHasNext] = useState(false);
-    const [showMore,setShowMore] = useState(false)
-    const [loading,setLoading] = useState(false)
+    const [showMore, setShowMore] = useState(false)
+    const [loading, setLoading] = useState(false)
     useEffect(() => {
         setLoading(true)
         fetch(`${BACKEND_URL}/api/product/trending/?page=${page}`)
@@ -51,38 +52,41 @@ function TrendingProducts() {
             </div>
             <h3 className="text-center text-3xl font-bold uppercase">Our Trending Products</h3>
             {
-                loading ? <Loading/> : (
+                loading ? <Loading /> : (
                     <div className="grid grid-cols-1 lg:grid-cols-4 gap-2">
-                    {showMore ? (
-                        
-                        products.map((data, index) => (
-                            <TrendingProductCard
-                                key={index}
-                                id={data.product.product.product_id}
-                                name={data.product.product.name}
-                                description={data.product.product.description}
-                                image={data.product.product.product_images[0].product_image}
-                                selling_price={data.product.selling_price}
-                            />
-                        ))
-                    ) : (
-                        products.slice(0, 8).map((data, index) => (
-                            <TrendingProductCard
-                                key={index}
-                                name={data.product.product.name}
-                                description={data.product.product.description}
-                                image={data.product.product.product_images[0].product_image}
-                                selling_price={data.product.selling_price}
-                                id={data.product.product.product_id}
+                        {showMore ? (
+
+                            products.map((data, index) => (
+                                <TrendingProductCard
+                                    key={index}
+                                    id={data.product.product.product_id}
+                                    name={data.product.product.name}
+                                    description={data.product.product.description}
+                                    image={data.product.product.product_images[0].product_image}
+                                    selling_price={data.product.selling_price}
+                                    rating={data.product.product.average_rating}
+
+                                />
+                            ))
+                        ) : (
+                            products.slice(0, 8).map((data, index) => (
+                                <TrendingProductCard
+                                    key={index}
+                                    name={data.product.product.name}
+                                    description={data.product.product.description}
+                                    image={data.product.product.product_images[0].product_image}
+                                    selling_price={data.product.selling_price}
+                                    id={data.product.product.product_id}
+                                    rating={data.product.product.average_rating}
 
 
-                            />
-                        ))
-                    )}
+                                />
+                            ))
+                        )}
                     </div>
                 )
             }
-      
+
             <div className="mt-4 flex justify-center">
                 <button onClick={() => setShowMore((prev) => !prev)} className='px-12 bg-red-500 py-3 font-bold text-white hover:bg-red-600 rounded-lg'>
                     {showMore ? "Show Less" : "Show More"}
@@ -92,29 +96,40 @@ function TrendingProducts() {
     );
 }
 
-const TrendingProductCard = ({ name, description, image,selling_price,id }) => {
+const TrendingProductCard = ({ name, description, image, selling_price, id, rating }) => {
     return (
-        <Suspense fallback={<Loading/>}>
+        <Suspense fallback={<Loading />}>
             <div className="bg-white rounded-lg overflow-hidden shadow-lg">
-            <img
-                className="w-full h-72 object-contain"
-                src={`${BACKEND_URL}${image}`}
-                alt="Product Primary Image"
-            />
-            <div className="p-6">
-                <h3 className="text-gray-900 font-semibold text-xl mb-2">{name}</h3>
-                <p className="text-gray-700 text-base mb-4">{description.slice(0, 72)}...</p>
-        <p className="text-red-500 text-xl font-semibold">₹{selling_price}</p>
-              <Link to="/purchase" state={{id:id}}>
-              <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">
-                    Show More
-                </button>
-              </Link>
-            </div>
+                <img
+                    className="w-full h-72 object-contain"
+                    src={`${BACKEND_URL}${image}`}
+                    alt="Product Primary Image"
+                />
+                <div className="p-6">
+                    <h3 className="text-gray-900 font-semibold text-xl mb-2">{name}</h3>
+                    <p className="text-gray-700 text-base mb-4">{description.slice(0, 72)}...</p>
+                    <p className="text-red-500 text-xl font-semibold">₹{selling_price}</p>
+                    <div className="flex items-center justify-between">
+                        {/* product rating */}
+                        <div className="flex items-center text-yellow-400">
+                            {Array.from({ length: rating }).map((_index) => (
+                                <IoStarSharp className='text-lg' />
+                            ))}
+                            <span className='text-black'>({rating})</span>
+                        </div>
+                        {/* open product in purchase page */}
+                        <Link to="/purchase" state={{ id: id }}>
+                            <button className="mt-4 px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 transition duration-300 ease-in-out">
+                                Show More
+                            </button>
+
+                        </Link>
+                    </div>
+                </div>
             </div>
         </Suspense>
 
-      
+
     );
 };
 
