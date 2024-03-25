c7dd6f5ba6fe6fd701b95708ddab075266681de2
adarshswaminath <adarshswaminath7@gmail.com>
2024-03-26 02:44:08 +0530

    feat: profile page cart worth price added

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -12,6 +12,7 @@ function Profile() {
 
     const [profile, setProfile] = useState({});
     const [cartLength, setCartLength] = useState(0);
+    const [cartTotalPrice,setCartTotalPrice] = useState(0)
     const [modalShow, setModalShow] = useState(false);
     const [address, setAddress] = useState("");
     const [error, setError] = useState(null);
@@ -44,7 +45,7 @@ function Profile() {
                 throw new Error("Failed to fetch user profile");
             }
             const data = await response.json();
-            setProfile(data);
+            setProfile(data)
             setAddress(data.address || ""); // Set address from profile data
         } catch (error) {
             setError("Failed to fetch user profile. Please try again later.");
@@ -63,6 +64,7 @@ function Profile() {
                 throw new Error("Failed to fetch cart products");
             }
             const data = await response.json();
+            setCartTotalPrice(data.subtotal_price)
             setCartLength(data.count);
         } catch (error) {
             setError("Failed to fetch cart products. Please try again later.");
@@ -95,10 +97,10 @@ function Profile() {
             }).then((res) => window.location.reload())
         } catch (error) {
             setShowErrorModal(true)
-            setError("Some Thing Went Wrong Try Again After Login")
-            setTimeout(() => {
-                navigate("/login")
-            }, 3000)
+            // setError("Some Thing Went Wrong Try Again After Login")
+            // setTimeout(() => {
+            //     navigate("/login")
+            // }, 3000)
             setShowErrorModal(false)
 
         }
@@ -165,7 +167,8 @@ function handleNewAddress() {
                         <ProfileItem title="Name" content={profile.name} />
                         <ProfileItem title="Email" content={profile.email} />
                         <ProfileItem title="Phone" content={profile.phone_number} />
-                        <ProfileItem title="Cart" content={`${cartLength} Products`} />
+                        <ProfileItem title="Cart" content={`${cartLength} Products `} />
+                        <ProfileItem title="Total" content={`₹${cartTotalPrice} /- `} />
                     </div>
                     <div className="border-t border-gray-200 pt-4">
                         <div className="flex items-center justify-between">
@@ -230,70 +233,101 @@ function handleNewAddress() {
 
                 {/* modal for adding address */}
                 {modalShow && (
-                    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-2 z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-2 z-50">
+        <div className="p-3 bg-white w-4xl">
+            <div className="flex items-center justify-between">
+                <h3 className="font-bold text-xl">Add Your Address</h3>
+                <button className="hover:rotate-45 rotate-0 transition duration-300 ease-in-out mb-2 bg-red-500 text-white rounded-full text-xl p-2" onClick={() => setModalShow(false)}>
+                    <IoMdClose />
+                </button>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-2 gap-3">
+                <input
+                    name="region"
+                    value={newAddress.region}
+                    onChange={handleNewAddressInput}
+                    type="text"
+                    className="input bg-gray-200"
+                    placeholder="Region"
+                />
+                <input
+                    name="state"
+                    value={newAddress.state}
+                    onChange={handleNewAddressInput}
+                    type="text"
+                    className="input bg-gray-200"
+                    placeholder="State"
+                />
+                <input
+                    name="district"
+                    value={newAddress.district}
+                    onChange={handleNewAddressInput}
+                    type="text"
+                    className="input bg-gray-200"
+                    placeholder="District"
+                />
+                <input
+                    name="postal_code"
+                    value={newAddress.postal_code}
+                    onChange={(e) => {
+                        const value = e.target.value;
+                        if (/^\d{0,6}$/.test(value)) {
+                            setNewAddress((prevState) => ({
+                                ...prevState,
+                                postal_code: value
+                            }));
+                        }
+                    }}
+                    onBlur={async () => {
+                        try {
+                            const response = await fetch(`https://api.postalpincode.in/pincode/${newAddress.postal_code}`);
+                            const data = await response.json();
+                            if (data[0]?.Status === 'Success') {
+                                setError('');
+                            } else {
+                                setError('Invalid pin code');
+                            }
+                        } catch (error) {
+                            console.error('Error checking pin code:', error);
+                            setError('Failed to validate pin code. Please try again.');
+                        }
+                    }}
+                    type="number"
+                    maxLength={6}
+                    className="input bg-gray-200"
+                    placeholder="Pin Code"
+                />
+                <input
+                    name="street_address"
+                    value={newAddress.street_address}
+                    onChange={handleNewAddressInput}
+                    type="text"
+                    className="input bg-gray-200"
+                    placeholder="Street Address"
+                />
+            </div>
+            {error && <p className="text-red-500">{error}</p>}
+            <div className="flex items-center p-2 gap-x-3">
+                <input
+                    type="checkbox"
+                    name="is_default"
+                    value={newAddress.is_default}
+                    onChange={(event) => setNewAddress((prevState) => ({
+                        ...prevState,
+                        is_default: event.target.checked
+                    }))}
+                    className="checkbox bg-gray-200"
+                />
+                <p className="font-bold">Use This Address As Default One</p>
+            </div>
+            <button
+                onClick={handleNewAddress}
+                className="mt-4 btn bg-red-500 hover:bg-red-600 text-white w-full">Submit
+            </button>
+        </div>
+    </div>
+)}
 
-                        <div className="p-3 bg-white  w-full">
-                            <div className="flex items-center justify-between">
-                                <h3 className="font-bold text-xl">Add Your Address</h3>
-                                <button className="hover:rotate-45 rotate-0 transition duration-300 ease-in-out mb-2 bg-red-500 text-white rounded-full text-xl p-2" onClick={() => setModalShow(false)}>
-                                <IoMdClose/>
-                                </button>
-                            </div>
-                            <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-12 gap-3">
-                                <input 
-                                    name="street_address" 
-                                    value={newAddress.street_address} 
-                                    onChange={handleNewAddressInput} 
-                                    type="text" 
-                                    className="input bg-gray-200" 
-                                    placeholder="Street Address" />
-                                <input 
-                                    name="region" 
-                                    value={newAddress.region} 
-                                    onChange={handleNewAddressInput} 
-                                    type="text" 
-                                    className="input bg-gray-200" 
-                                    placeholder="Region" />
-                                <input
-                                    name="district"
-                                    value={newAddress.district}
-                                    onChange={handleNewAddressInput} 
-                                    type="text" className="input bg-gray-200" 
-                                    placeholder="District" />
-                                <input 
-                                    name="state" 
-                                    value={newAddress.state}
-                                    onChange={handleNewAddressInput}
-                                    type="text" 
-                                    className="input bg-gray-200" 
-                                    placeholder="State" />
-                            </div>
-                            <input 
-                                name="postal_code"
-                                value={newAddress.postal_code}
-                                onChange={handleNewAddressInput}
-                                type="number" 
-                                className="bg-gray-200 input w-full mt-4" 
-                                placeholder="Postal Code" />
-                                <div className="flex items-center p-2 gap-x-3">
-                                    <input 
-                                        type="checkbox" 
-                                        name="is_default"
-                                        value={newAddress.is_default} 
-                                        onChange={(event) => setNewAddress(prevState => ({
-                                            ...prevState,
-                                            is_default: event.target.checked
-                                        }))}
-                                        className="checkbox bg-gray-200"
-                                        />
-                                        <p className="font-bold">Use This Address As Default One</p>
-                                </div>
-                            <button 
-                                onClick={handleNewAddress} 
-                                className="mt-4 btn bg-red-500 hover:bg-red-600 text-white w-full">Submit</button>
-                        </div>
-                    </div>
-                )}
                 {/* end of modal for getting address */}
                 <div className="px-6 py-4 bg-gray-100 border-t border-gray-200 flex justify-end">
                     <button onClick={() => {
