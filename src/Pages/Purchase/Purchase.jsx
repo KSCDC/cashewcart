2b3c2ebbe4536fcdf9438682e2c09658b7320a18
adarshswaminath <adarshswaminath7@gmail.com>
2024-03-28 01:38:36 +0530

    fix: dynamic route

--- src/Pages/Purchase/Purchase.jsx
+++ src/Pages/Purchase/Purchase.jsx
@@ -10,44 +10,49 @@ import SponsorProducts from '../Home/SponsorProducts';
 import NotFound from '../../Components/NotFound';
 
 function Purchase() {
-  const [response,setResponse] = useState(null) // Initialize response state as null
-  const [isLoading,setIsLoading] = useState(false)
+  const [response, setResponse] = useState(null) // Initialize response state as null
+  const [isLoading, setIsLoading] = useState(false)
   // Extract the productId from URL parameters
   const { productId } = useParams();
 
   useEffect(() => {
     // get into the top side of the page
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0)
     setIsLoading(true)
     // Use the productId in the API call
     fetch(`${BACKEND_URL}/api/product/detail/${productId}/`)
-    .then((res) => res.json())
-    .then((data) => setResponse(data))
-    .catch((err) => {
-      console.log("API CALL FAILED");
-      setResponse(null); // Set response to null if API call fails
-    })
-    .finally(() => setIsLoading(false)); // Ensure setIsLoading(false) is called even if there's an error
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setResponse(data)
+      })
+      .catch((err) => {
+        console.log("API CALL FAILED");
+        setResponse({ errors: { detail: "Not found." } }); // Set response with error object if API call fails
+      })
+      .finally(() => setIsLoading(false)); // Ensure setIsLoading(false) is called even if there's an error
   }, [productId]); // Add productId as dependency
 
+  // Render conditionally based on response and isLoading states
   return (
     <main className="min-h-screen">
-        {/* Conditionally render based on response */}
-        {isLoading && <Loading />}
-        {!isLoading && response && response.id ? (
-          <>
-            <PurchasePage {...response}/>
-            <BestSellers/>
-            <Banner image={`/banner/hero-${Math.floor(Math.random() * 5) + 1}.png`} />
-            <TrendingProducts/>
-            <Banner image={`/banner/hero-${Math.floor(Math.random() * 5) + 1}.png`} />
-            <SponsorProducts/>
-          </>
-        ) : (
-          <div className='flex justify-center'>
-            <NotFound/>
-          </div>
-        )}
+      {isLoading && <Loading />}
+      {/* Render if response exists and it contains a valid product */}
+      {!isLoading && response && !response.errors ? (
+        <>
+          <PurchasePage {...response}/>
+          <BestSellers/>
+          <Banner image={`/banner/hero-${Math.floor(Math.random() * 5) + 1}.png`} />
+          <TrendingProducts/>
+          <Banner image={`/banner/hero-${Math.floor(Math.random() * 5) + 1}.png`} />
+          <SponsorProducts/>
+        </>
+      ) : (
+        // Render if response is null or productId is invalid
+        <div className="flex justify-center">
+          <NotFound/>
+        </div>
+      )}
     </main>
   )
 }
